923d36bd69a8512b50eca54a40d2834bd137607c
Raul Canada <fagunn2@gmail.com>
2022-11-24 08:37:16 +0100

    validacion de formularios2

---
+++ my-app/src/blog/CrearCuenta.jsx
@@ -0,0 +1,131 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useForm } from "react-hook-form";
+import { color } from '@mui/system';
+import { red } from '@mui/material/colors';
+import { DialogContent,Typography } from "@mui/material";
+
+
+const URI = 'http://localhost:9000/blogs/'
+
+const CompCreateCuenta = () => {
+
+    //Expresiones regulares
+    
+    const patterns = { dni: /^\d{8}[a-zA-Z]$/};
+    
+    const messages = {
+        req: "Este campo es obligatorio",
+        dni: "El formato introducido no es el correcto",
+        //mail: "Debes introducir una dirección correcta",
+        //phone: "Debes introducir un número correcto"
+    };
+
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const store = async (userInfo) => {
+
+
+        await axios.post(URI, { dni: userInfo.dni, name: userInfo.name, last_name: userInfo.last_name, birthday: userInfo.birthday, phone: userInfo.phone, email: userInfo.email, password: userInfo.password })
+
+
+    }
+
+
+    return (
+        <div>
+            <h3>CREAR REGISTRO</h3>
+            <form onSubmit={handleSubmit(store)}>
+                <div className='mb-3' >
+                    <label className='form-label'>DNI</label>
+                    <input name='dni'
+                        {...register("dni", { required: messages.req ,
+                            pattern: {
+                            value: patterns.dni,
+                            message: messages.dni}
+                          })}
+
+                        type="text"
+                        className='form-control'
+                    />
+                   
+                   <Typography sx={{color:'red'}}> {errors.dni && errors.dni.message}</Typography>
+                   
+                    
+                    
+                </div>
+                <div className='mb-3'>
+                    <label className='form-label'>Nombre</label>
+                    <input name='name'
+                        {...register("name", { required: messages.req })}
+
+                        type="text"
+                        className='form-control'
+                    />
+                   <Typography sx={{color:'red'}}>{errors.name && errors.name.message}</Typography>
+
+                    <div className='mb-3'>
+                        <label className='form-label'>Apellido</label>
+                        <input name='last_name'
+                            {...register("last_name", { required: messages.req })}
+
+                            type="text"
+                            className='form-control'
+                        />
+                        <Typography sx={{color:'red'}}>{errors.last_name && errors.last_name.message }</Typography>
+
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label'>Cumple</label>
+                        <input name='birthday'
+                            {...register("birthday", { required: messages.req })}
+
+                            type="date"
+                            className='form-control'
+                        />
+                       <Typography sx={{color:'red'}}> {errors.birthday && errors.birthday.message}</Typography>
+
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label'>Telefono</label>
+                        <input name='phone'
+                            {...register("phone", { required: messages.req })}
+                            type="text"
+                            className='form-control'
+                        />
+                       <Typography sx={{color:'red'}}> {errors.phone && errors.phone.message}</Typography>
+
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label'>email</label>
+                        <input name='email'
+                            {...register("email", { required: messages.req })}
+
+                            type="email"
+                            className='form-control'
+                        />
+                        <Typography sx={{color:'red'}}>{errors.email && errors.email.message }</Typography>
+
+                    </div>
+                    <div className='mb-3'>
+                        <label className='form-label'>Password</label>
+                        <input name='password'
+                            {...register("password", { required: messages.req })}
+
+                            type="password"
+                            className='form-control'
+                        />
+                      <Typography sx={{color:'red'}}> {errors.password && errors.password.message}</Typography> 
+                    </div>
+                </div>
+                <button type='submit' className='btn btn-primary'>Crear</button>
+            </form>
+            {/*!valid&&<span>Data no valido</span>*/}
+        </div>
+    )
+
+}
+
+export default CompCreateCuenta
--- my-app/src/blog/CrearLog.js
+++ my-app/src/blog/CrearLog.js
@@ -1,49 +1,72 @@
 import axios from 'axios'
-import {useState} from 'react'
+import { useState } from 'react'
+import { useForm } from "react-hook-form";
+import { DialogContent,Typography } from "@mui/material";
 
 const URI = 'http://localhost:9000/entrada/'
 
 const CompCreateLog = () => {
-    const [name, setName] = useState('')
-    const [password, setPassword] = useState('')
+    /*const [name, setName] = useState('')
+    const [password, setPassword] = useState('')*/
+
+    const messages = {
+        req: "Este campo es obligatorio",
+        // name: "El formato introducido no es el correcto",
+        //mail: "Debes introducir una dirección correcta",
+        //phone: "Debes introducir un número correcto"
+    };
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const store = async (userInfo) => {
+
+
+        await axios.post(URI, { name: userInfo.name, password: userInfo.password })
+
+
+    }
 
 
     //procedimiento guardar
-const store = async (e) => {
-    e.preventDefault()
-   await  axios.post (URI,{name:name,password:password})
-  /* navigate('/')*/
-}
+    /*const store = async (e) => {
+        e.preventDefault()
+       await  axios.post (URI,{name:name,password:password})
+      /* navigate('/')*/
+    //}
 
 
-        return(
-            <div>
-                <h3>LOGIN</h3>
-                <form onSubmit={store}>
-                   
-                    <div className='mb-3'>
-                    <label  className='form-label'>Nombre</label>
-                    <input
-                        value={name}
-                        onChange={ (e)=> setName(e.target.value)}
+    return (
+        <div>
+            <h3>LOGIN</h3>
+            <form onSubmit={handleSubmit(store)}>
+
+                <div className='mb-3'>
+                    <label className='form-label'>Nombre</label>
+                    <input name='name'
+                        {...register("name", { required: messages.req })}
+                        //value={name}
+                        //onChange={(e) => setName(e.target.value)}
                         type="text"
                         className='form-control'
-                        />   
-                   
+                    />
+                    <Typography sx={{color:'red'}}>{errors.name && errors.name.message}</Typography>
+
                     <div className='mb-3'>
-                    <label className='form-label'>Password</label>
-                    <input
-                        value={password}
-                        onChange={ (e)=> setPassword(e.target.value)}
-                        type="text"
-                        className='form-control'
+                        <label className='form-label'>Password</label>
+                        <input name='password'
+                            //value={password}
+                            {...register("password", { required: messages.req })}
+                          //  onChange={(e) => setPassword(e.target.value)}
+                            type="text"
+                            className='form-control'
                         />
+                       <Typography sx={{color:'red'}}> {errors.password && errors.password.message}</Typography>
                     </div>
-                    </div>
-                    <button type='submit' className='btn btn-primary'>LOGAR</button>
-                </form>
-            </div>
-        )
+                </div>
+                <button type='submit' className='btn btn-primary'>LOGAR</button>
+            </form>
+        </div>
+    )
 
 }
 
